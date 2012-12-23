240dbefb0792abf080ef48b83be80de411adc0eb
Nicolas Vanhoren <nicolas.vanhoren.01@gmail.com>
2012-12-24 00:02:41 +0100

    Made more tests

--- bisync_test.py
+++ bisync_test.py
@@ -42,14 +42,109 @@ class TestSequenceFunctions(unittest.TestCase):
 
     def test_first_sync(self):
         s1 = TestSource({
-            "file1": ["1", "1"],
+            "file1": [[True, "1", "1"]],
         })
         s2 = TestSource({
         })
         sync = bisync.Synchronizer()
         sync.synchronize_all([s1, s2])
-        self.assertEqual(s1.test_index, s1.index)
-        self.assertEqual(s1.test_index, s2.index)
+        result = {
+            "file1": [[True, "1", "1"]],
+        }
+        self.assertEqual(s1.index, result)
+        self.assertEqual(s2.index, result)
+
+    def test_updated_version(self):
+        s1 = TestSource({
+            "file1": [[True, "1", "1"], [True, "1", "2"]],
+        })
+        s2 = TestSource({
+            "file1": [[True, "1", "1"]],
+        })
+        sync = bisync.Synchronizer()
+        sync.synchronize_all([s1, s2])
+        result = {
+            "file1": [[True, "1", "1"], [True, "1", "2"]],
+        }
+        self.assertEqual(s1.index, result)
+        self.assertEqual(s2.index, result)
+
+    def test_deleted_file(self):
+        s1 = TestSource({
+            "file1": [[True, "1", "1"], [False]],
+        })
+        s2 = TestSource({
+            "file1": [[True, "1", "1"]],
+        })
+        sync = bisync.Synchronizer()
+        sync.synchronize_all([s1, s2])
+        result = {
+            "file1": [[True, "1", "1"], [False]],
+        }
+        self.assertEqual(s1.index, result)
+        self.assertEqual(s2.index, result)
+
+    def test_moved_file(self):
+        s1 = TestSource({
+            "file1": [[True, "1", "1"], [False]],
+            "file2": [[True, "1", "1"]],
+        })
+        s2 = TestSource({
+            "file1": [[True, "1", "1"]],
+        })
+        sync = bisync.Synchronizer()
+        sync.synchronize_all([s1, s2])
+        result = {
+            "file1": [[True, "1", "1"], [False]],
+            "file2": [[True, "1", "1"]],
+        }
+        self.assertEqual(s1.index, result)
+        self.assertEqual(s2.index, result)
+
+    def test_conflict(self):
+        s1 = TestSource({
+            "file1": [[True, "1", "1"], [True, "1", "3"]],
+        })
+        s2 = TestSource({
+            "file1": [[True, "1", "1"], [True, "1", "2"]],
+        })
+        sync = bisync.Synchronizer()
+        sync.synchronize_all([s1, s2])
+        result = {
+            "file1": [[True, "1", "1"], [True, "1", "2"], [True, "1", "3"]],
+        }
+        self.assertEqual(s1.index, result)
+        self.assertEqual(s1.index, result)
+
+    def test_conflict_one_delete(self):
+        s1 = TestSource({
+            "file1": [[True, "1", "1"], [True, "1", "3"], [False]],
+        })
+        s2 = TestSource({
+            "file1": [[True, "1", "1"], [True, "1", "2"]],
+        })
+        sync = bisync.Synchronizer()
+        sync.synchronize_all([s1, s2])
+        result = {
+            "file1": [[True, "1", "1"], [True, "1", "3"], [False], [True, "1", "2"]],
+        }
+        self.assertEqual(s1.index, result)
+        self.assertEqual(s1.index, result)
+
+    def test_conflict_two_delete(self):
+        s1 = TestSource({
+            "file1": [[True, "1", "1"], [True, "1", "3"], [False]],
+        })
+        s2 = TestSource({
+            "file1": [[True, "1", "1"], [True, "1", "2"], [False]],
+        })
+        sync = bisync.Synchronizer()
+        sync.synchronize_all([s1, s2])
+        result = {
+            "file1": [[True, "1", "1"], [True, "1", "3"], [False], [True, "1", "2"], [False]],
+        }
+        self.assertEqual(s1.index, result)
+        self.assertEqual(s1.index, result)
 
 if __name__ == '__main__':
     unittest.main()
